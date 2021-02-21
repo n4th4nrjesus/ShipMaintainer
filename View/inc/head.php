ebe115aadf1e3225f8fee8a8a5ee353cd1aa8178
n4th4nrjesus <36750209+n4th4nrjesus@users.noreply.github.com>
2021-02-21 13:41:32 -0300

    continuação do menu principal

--- View/inc/head.php
+++ View/inc/head.php
@@ -6,7 +6,8 @@
     <meta charset="utf-8">
     <title>Ship Maintainer <?= fetchRouteTitle($_SERVER['REQUEST_URI']) ?></title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,500;0,700;1,700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./View/css/main.css">
 </head>
 
--- View/index.php
+++ View/index.php
@@ -1,1 +1,20 @@
-<h1>Menu principal</h1>
+<div class="container">
+    <div class="content">
+        <div class="box row">
+            <div class="col-12">
+                <h1 class="box-title">Ship Maintainer</h1>
+            </div>
+            <div class="col-12 col-lg-6 box-separator">
+                <h2 class="box-subtitle">Rápido! Os aliens estão invadindo!</h2>
+                <p class="box-text">
+                    Você precisa tomar conta do reparo de toda a nave, mas ao mesmo tempo existem esses <b class="text-diff">alienígenas</b> invadindo ela.
+                    <br><br>
+                    Os tripulantes estão ocupados com outros afazeres, então você também precisa <b class="text-diff">protegê-los</b>, pois eles não podem parar.
+                </p>
+                <p class="box-text attractive-text">Rápido, venha controlar essa situação!</p>
+            </div>
+            <div class="col-12 col-lg-6">
+            </div>
+        </div>
+    </div>
+</div>
